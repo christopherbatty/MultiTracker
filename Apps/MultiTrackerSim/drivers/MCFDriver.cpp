077e23159b78c3792481e16ceac87baa9f82e7e3
David Cha <djc991006@gmail.com>
2023-10-15 23:03:14 +0100

    Fix MCF case stepping:
        - Substeps were not accumulating vertex positions as each substep
          would read from the old positions vector and write the calculated
          positions into the new positions vector. Instead, substeps after
          the first one now read from the new positions vector.
    
        - Vertex velocities were being unintentionally accumulated as this
          vector was not zeroed out at each substep.
    
        - Use new positions, not old positions, in various other
          calculations for substeps to be correct.
    
        - Handle floating point imprecision.
    
        - Handle case where the norm of the cross product of the vectors
          of a triangle can be zero.
    
        - Use correct equation for detemining max time step allowed for
          limiting how much an edge can shrink by.

--- Apps/MultiTrackerSim/drivers/MCFDriver.cpp
+++ Apps/MultiTrackerSim/drivers/MCFDriver.cpp
@@ -16,6 +16,9 @@ bool MCFDriver::step(SurfTrack * st, double dt, bool bbwall)
     // adaptive time stepping
     double dt_left = dt;
     double dt_largest_possible = 0;
+
+    // set newpositions to position
+    st->set_all_newpositions(st->get_positions());
     
     int substep_count = 0;
     while (dt_left > 0 && substep_count < 100)
@@ -26,22 +29,40 @@ bool MCFDriver::step(SurfTrack * st, double dt, bool bbwall)
         dt_largest_possible = determineMaxDt(st, v);
         std::cout << "dt_left = " << dt_left << " max dt = " << dt_largest_possible << std::endl;
         if (dt_largest_possible >= dt_left)
-            dt_largest_possible = dt_left;
+        {
+            dt_largest_possible = dt_left; // clamp dt_largest_possible
+
+            // Setting dt_left to 0 manually as otherwise floating point precision
+            // problems from doing dt_left -= dt_largest_possible can cause
+            // dt_left to be a very small number near zero leading to some issues.
+            dt_left = 0;
+        } else
+        {
+            dt_left -= dt_largest_possible;
+        }
 
         for (size_t i = 0; i < st->m_mesh.nv(); i++)
         {
-           if (st->m_mesh.vertex_is_deleted(i)) {
-              st->set_newposition(i, st->get_position(i));
-              continue;
-           }
-            Vec3d m = st->m_masses[i];
-            Vec3d newpos = st->get_position(i) + Vec3d(v[i][0] / m[0], v[i][1] / m[1], v[i][2] / m[2]) * dt_largest_possible;
+            if (st->m_mesh.vertex_is_deleted(i))
+                continue;
+
+            Vec3d newpos = st->get_newposition(i);
+
+            Vec3d m(1, 1, 1);
+            if (bbwall)
+            {
+                const int onwall = onBBWall(newpos);
+                m[0] = ((onwall & (1 << 0)) || (onwall & (1 << 3)) ? std::numeric_limits<double>::infinity() : 1);
+                m[1] = ((onwall & (1 << 1)) || (onwall & (1 << 4)) ? std::numeric_limits<double>::infinity() : 1);
+                m[2] = ((onwall & (1 << 2)) || (onwall & (1 << 5)) ? std::numeric_limits<double>::infinity() : 1);
+            }
+
+            newpos += Vec3d(v[i][0] / m[0], v[i][1] / m[1], v[i][2] / m[2]) * dt_largest_possible;
             assert(newpos == newpos);
             if (bbwall) newpos = Vec3d(std::min(1.0, std::max(0.0, newpos[0])), std::min(1.0, std::max(0.0, newpos[1])), std::min(1.0, std::max(0.0, newpos[2])));
             st->set_newposition(i, newpos);
         }
         
-        dt_left -= dt_largest_possible;
         substep_count++;
     }
     
@@ -56,59 +77,89 @@ bool MCFDriver::step(SurfTrack * st, double dt, bool bbwall)
     }
 }
 
-double MCFDriver::determineMaxDt(SurfTrack * st, std::vector<Vec3d> & v)
+double MCFDriver::determineMaxDt(const SurfTrack * st, const std::vector<Vec3d> & v)
 {
     double global_max_dt = 1.0;
     for (size_t i = 0; i < st->m_mesh.ne(); i++)
     {
        if (st->m_mesh.edge_is_deleted(i))
           continue;
-        size_t v0 = st->m_mesh.m_edges[i][0];
-        size_t v1 = st->m_mesh.m_edges[i][1];
-        Vec3d edge = st->get_position(v1) - st->get_position(v0);
-        Vec3d rv = v[v1] - v[v0];
-        double approaching_velocity = -dot(rv, edge) / dot(edge, edge);
-        
-        double max_dt = 0;
-        if (approaching_velocity <= 0)
-            max_dt = std::numeric_limits<double>::infinity();
-        else
-            max_dt = 1 / approaching_velocity * 0.8;    // allow the edge to be shrunk by 80% in one time step at most
-        
-        if (max_dt < global_max_dt)
-            global_max_dt = max_dt;
+        const size_t v0 = st->m_mesh.m_edges[i][0];
+        const size_t v1 = st->m_mesh.m_edges[i][1];
+        const Vec3d edge = st->get_newposition(v1) - st->get_newposition(v0);
+        const Vec3d rv = v[v1] - v[v0];
+
+        const double alpha = 0.2; // allow the edge to be shrunk by at most 80% in one time substep
+        const double a = dot(rv, rv);
+        if (a < 1e-6) continue; // both endpoints have same velocity vectors
+        const double b = 2 * dot(rv, edge);
+        const double c = (1 - alpha*alpha) * dot(edge, edge);
+        const double discriminant = b*b - 4*a*c;
+
+        // If discriminant < 0 (no solutions) or discriminant = 0 (one solution),
+        // then there is no constraint on t. Else if discriminant > 0 (two solutions),
+        // and the smaller solution is nonnegative, then that solution is an
+        // upper bound on t. Note that in this case both solutions are nonnegative
+        // or nonpositive.
+        double max_dt = std::numeric_limits<double>::infinity();
+        if (discriminant > 1e-6)
+        {
+            const double smaller_root = (-b - std::sqrt(discriminant)) / (2*a);
+            max_dt = smaller_root < 0 ? std::numeric_limits<double>::infinity() : smaller_root;
+        }
+
+        global_max_dt = std::min(global_max_dt, max_dt);
     }
     
     return global_max_dt;
 }
 
-void MCFDriver::evaluateV(LosTopos::SurfTrack * st, std::vector<LosTopos::Vec3d> & v)
+void MCFDriver::evaluateV(const LosTopos::SurfTrack * st, std::vector<LosTopos::Vec3d> & v)
 {
     const double COEF = 1;
     
     v.resize(st->m_mesh.nv(), Vec3d(0, 0, 0));
+    std::fill(v.begin(), v.end(), Vec3d(0, 0, 0));
+
     for (size_t i = 0; i < st->m_mesh.nt(); i++)
     {
        if (st->m_mesh.triangle_is_deleted(i))
           continue;
 
-        Vec3d p0 = st->get_position(st->m_mesh.m_tris[i][0]);
-        Vec3d p1 = st->get_position(st->m_mesh.m_tris[i][1]);
-        Vec3d p2 = st->get_position(st->m_mesh.m_tris[i][2]);
+        const Vec3d p0 = st->get_newposition(st->m_mesh.m_tris[i][0]);
+        const Vec3d p1 = st->get_newposition(st->m_mesh.m_tris[i][1]);
+        const Vec3d p2 = st->get_newposition(st->m_mesh.m_tris[i][2]);
         
-        Vec3d v01 = p1 - p0;
-        Vec3d v20 = p0 - p2;
+        const Vec3d v01 = p1 - p0;
+        const Vec3d v20 = p0 - p2;
         
-        Vec3d A = cross(v01, -v20);
-        double Anorm = mag(A);
-        Vec3d mul = A / Anorm;
+        const Vec3d A = cross(v01, -v20);
+        const double Anorm = mag(A);
+        const Vec3d mul = A / Anorm;
+        if ((Anorm == 0) || (mul != mul)) // cross product may sometimes be 0 due to degenerate triangles
+            continue;
         
-        Vec3d p2part = COEF * cross(v01, mul);
-        Vec3d p1part = COEF * cross(mul, -v20);
-        Vec3d p0part = -(p1part + p2part);
+        const Vec3d p2part = COEF * cross(v01, mul);
+        const Vec3d p1part = COEF * cross(mul, -v20);
+        const Vec3d p0part = -(p1part + p2part);
 
         v[st->m_mesh.m_tris[i][0]] += p0part;
         v[st->m_mesh.m_tris[i][1]] += p1part;
         v[st->m_mesh.m_tris[i][2]] += p2part;
     }
+}
+
+int MCFDriver::onBBWall(const Vec3d & pos)
+{
+    static const double WALL_THRESHOLD = 1e-6;
+    
+    int walls = 0;
+    if (pos[0] < 0 + WALL_THRESHOLD) walls |= (1 << 0);
+    if (pos[1] < 0 + WALL_THRESHOLD) walls |= (1 << 1);
+    if (pos[2] < 0 + WALL_THRESHOLD) walls |= (1 << 2);
+    if (pos[0] > 1 - WALL_THRESHOLD) walls |= (1 << 3);
+    if (pos[1] > 1 - WALL_THRESHOLD) walls |= (1 << 4);
+    if (pos[2] > 1 - WALL_THRESHOLD) walls |= (1 << 5);
+    
+    return walls;
 }
